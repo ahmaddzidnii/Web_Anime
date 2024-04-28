f7773ba34052fba8af8ed79ab7e45b4425d5ffe4
ahmaddzidnii <zdngaming312@gmail.com>
2024-04-28 14:38:41 +0700

    chore: add sitemap.xml dan robots.txt

--- app/(anime)/anime/details/[id]/page.jsx
+++ app/(anime)/anime/details/[id]/page.jsx
@@ -2,6 +2,7 @@ import { GiPeriscope } from "react-icons/gi";
 
 import { getDetailAnimeById } from "@/services/api";
 import { DetailAnimePage } from "./detail-anime-page";
+import { notFound } from "next/navigation";
 
 export async function generateMetadata({ params }) {
   const detailsAnime = await getDetailAnimeById(params.id);
@@ -13,20 +14,21 @@ export async function generateMetadata({ params }) {
 const Page = async ({ params }) => {
   const detailsAnime = await getDetailAnimeById(params.id);
 
-  if (!detailsAnime)
+  if (!detailsAnime) {
     return (
       <div className="pt-5">
         <div className=" flex min-h-screen items-center justify-center gap-5">
           <GiPeriscope className="text-4xl" />
           <h1 className="mb-5 text-xl font-bold tracking-wide sm:text-2xl  md:text-3xl lg:text-4xl">
-            Tidak ada hasil untuk kata kunci ini.
+            Tidak ada hasil untuk id ini.
           </h1>
         </div>
       </div>
     );
+  }
 
   return (
-    <main className="pt-5">
+    <main className="min-h-screen pt-5">
       <DetailAnimePage api={detailsAnime} />
     </main>
   );
---
+++ app/sitemap.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export default async function sitemap() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const { data: animes } = await axios.get(
+    `https://api.jikan.moe/v4/top/anime?sfw=true`,
+  );
+
+  const animesEntries = animes.data.map((anime) => ({
+    url: `${baseUrl}/anime/details/${anime.mal_id}`,
+    lastModified: new Date(),
+  }));
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/anime`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/anime/top`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/anime/children`,
+      lastModified: new Date(),
+    },
+    ...animesEntries,
+  ];
+}
