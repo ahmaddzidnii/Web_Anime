991b6f98131cef1256a9f557783c4b50d8aad337
ahmaddzidnii <zdngaming312@gmail.com>
2024-05-16 19:21:28 +0700

    fix: middleware

--- middleware.js
+++ middleware.js
@@ -4,10 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/anime", "/anime/(.*)", "/api/webhooks/(.*)"],
-  ignoredRoutes: ["/anime/(.*)"],
+  publicRoutes: ["/api/webhooks/(.*)"],
+  ignoredRoutes: ["/anime/(.*)", "/", "/anime"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
